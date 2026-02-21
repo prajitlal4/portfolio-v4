6151bd28ab09f16a96d21065f5987a76276ce4f5
Prajit <prajit.lal4@gmail.com>
2026-02-21 10:09:15 +0800

    fixing forms

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -108,7 +108,7 @@ export default function ContactPage() {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('/', {
+      const response = await fetch('/__forms.html', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
--- src/app/websites-for-tradies/page.tsx
+++ src/app/websites-for-tradies/page.tsx
@@ -141,7 +141,7 @@ export default function WebsitesForTradiesPage() {
     setSubmitting(true);
 
     try {
-      const response = await fetch('/', {
+      const response = await fetch('/__forms.html', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams({
